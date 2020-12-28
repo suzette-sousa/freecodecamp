60817574a80f3f59b8fdfb845a337431113ed31b
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-28 16:14:20 +0100

    update exercise Title Case a Sentence

--- javascript-algorithms-and-data-structures/basic-algorithm-scripting/title-case-a-sentence.js
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/title-case-a-sentence.js
@@ -1,6 +1,6 @@
 function titleCase(str) {
-  let newStr = str.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1)).join(' ');
+  let newStr = str.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ');
   return newStr;
 }
 
-console.log(titleCase("I'm a little tea pot"));
+console.log(titleCase("sHoRt AnD sToUt"));
